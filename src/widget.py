8c6be6491b0b6678b13a787a457f3b2a0ae0370d
Grishina Valeria <shwscottholmes@gmail.com>
2026-03-23 21:41:43 +0300

    добавление функции для даты

--- src/widget.py
+++ src/widget.py
@@ -12,7 +12,7 @@ def mask_account_card(data_str: str) -> str:
     type_name = "".join(re.findall(r"\D+", data_str)).strip() # извлекаем только текст
 
 
-    if "счет" in type_name.lower():            # если будет ключевое слово "счет", то примениться маска для счета,
+    if "счет" in type_name.lower():            # если будет ключевое слово "счет", то применится маска для счета,
         masked_number = get_mask_account(numbers_only)   # если не будет ключевого слова, то применится маска
                                                                          # для номера карты
     else:
@@ -24,3 +24,20 @@ def mask_account_card(data_str: str) -> str:
 if __name__ == "__main__":
     user_input = input("Введите данные: ")
     print(mask_account_card(user_input))
+
+def get_date(date_string: str) -> str:
+    """
+    Преобразует строку "2024-03-11T..." с помощью срезов.
+    """
+
+    year = date_string[0:4] # извлекаем год
+
+    month = date_string[5:7] # извлекаем месяц
+
+    day = date_string[8:10] # извлекаем день
+
+
+    return f"{day}.{month}.{year}" # собираем в нужном порядке через точку
+
+test_date = "2024-03-11T02:26:18.671407"
+print(f"Дата: {get_date(test_date)}")
